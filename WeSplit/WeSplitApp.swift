8cb09bde841e55e5f5f4a902b575e4ca8502210f
SyncMainDev <javaftl@gmail.com>
2024-07-13 01:15:01 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Christopher Wade on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
